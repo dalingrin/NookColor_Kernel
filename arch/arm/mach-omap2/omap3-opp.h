539bf55e659c58ddc2968f9bdfcde6be927315ac
Dalingrin <dalingrin@gmail.com>
2011-03-31 21:55:09 -0500

    Overclock DSP at all OPPs. TUN driver in kernel

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -36,7 +36,7 @@ static struct omap_opp omap3630_dsp_rate_table[] = {
 	/*OPP4 (OPP-1G) - 1.35V*/
 	{S800M, VDD1_OPP4, 0x3c, 0x0, 0x0},
 	/*OPP5 (OPP-1.3G) - 1.35V*/
-	{S65M, VDD1_OPP5, 0x3c, 0x0, 0x0},
+	{S800M, VDD1_OPP5, 0x3c, 0x0, 0x0},
 };
 
 static struct omap_opp omap3621_mpu_rate_table[] = {
